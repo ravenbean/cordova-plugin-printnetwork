43cfe5701fe3b9d17ef9df0f2555a428eab4344c
ravenbean <taufanbean@gmail.com>
2019-02-21 10:10:10 +0700

    first upload

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+<a name="0.0.4"></a>
+# 0.0.3 (2019-01-22)
+
+##  Features
+* Added safety check while unregistering Broadcase receiver for USB connection/disconnection.
+
+<a name="0.0.3"></a>
+# 0.0.3 (2018-09-27)
+
+##  Features
+* As few proprietary device is not giving "Product Name" and other details, in those device, plugin is failing. Chanegd provide only bare minimum pritner details. Example "DeviceId and Vendor id".
+
+<a name="0.0.2"></a>
+# 0.0.2 (2018-09-21)
+
+##  Features
+* Added function to disconnect from already connected USB device
+
+<a name="0.0.1"></a>
+# 0.0.1 (2018-09-21)
+
+##  Features
+* Provided function to list all USB devices
+* Provided function to connect to more than one USB device and send print content to those printers
+* Provided function to cut paper, paper available check and send any POS command directly to the printer.
---
+++ www/usbprint.js
@@ -0,0 +1,29 @@
+module.exports = {
+    print: function(printername, msg, successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, "PrinterService", "print", [printername, msg]);
+    },
+    printWifi: function(msg, successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, "PrinterService", "printWifi", [msg]);
+    },
+    connectNetworkPrinters: function(msg, successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, "PrinterService", "connectNetworkPrinters", [msg]);
+    },
+    sendCommand: function(printername, command, successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, "PrinterService", "sendCommand", [printername, command]);
+    },
+    connect: function(printername, successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, "PrinterService", "connect", [printername]);
+    },
+    disconnect: function(printername, successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, "PrinterService", "disconnect", [printername]);
+    },
+    getConnectedPrinters: function(successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, "PrinterService", "getConnectedPrinters", []);
+    },
+    isPaperAvailable: function(printername, successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, "PrinterService", "isPaperAvailable", [printername]);
+    },
+    cutPaper: function(printername, successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, "PrinterService", "cutPaper", [printername]);
+    }
+};
